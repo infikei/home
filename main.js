babe7c67a21fd4d87c971d0a0ad3c4d25ef577e1
Infikei <infikei@naver.com>
2021-08-05 11:50:23 +0900

    up

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function link_home() {
-    location.href="https://infikei.github.io/home";
+    location.href="https://infikei.github.io/home/new";
 }
 function link_profile() {
-    location.href="https://infikei.github.io/home/#profile";
+    location.href="https://infikei.github.io/home/new/#profile";
 }
 function link_blog() {
-    location.href="https://infikei.github.io/home/#blog";
+    location.href="https://infikei.github.io/home/new/#blog";
 }
 function link_pages() {
-    location.href="https://infikei.github.io/home/#pages";
+    location.href="https://infikei.github.io/home/new/#pages";
 }
 function link_contact() {
-    location.href="https://infikei.github.io/home/#contact";
+    location.href="https://infikei.github.io/new/home/#contact";
 }
 function link_talk() {
     location.href="https://pf.kakao.com/_xaeEyT";
