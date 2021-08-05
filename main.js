177b0f73eccc0087174c2c0e4e794a4a37f6f115
Infikei <infikei@naver.com>
2021-08-06 06:58:17 +0900

    up

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ function link_home() {
     location.href="https://infikei.github.io/home/new";
 }
 function link_profile() {
-    location.href="https://infikei.github.io/home/new/#profile";
+    location.href="https://infikei.github.io/home/new#profile";
 }
 function link_blog() {
-    location.href="https://infikei.github.io/home/new/#blog";
+    location.href="https://infikei.github.io/home/new#blog";
 }
 function link_pages() {
-    location.href="https://infikei.github.io/home/new/#pages";
+    location.href="https://infikei.github.io/home/new#pages";
 }
 function link_contact() {
-    location.href="https://infikei.github.io/home/new/#contact";
+    location.href="https://infikei.github.io/home/new#contact";
 }
 function link_talk() {
     window.open("https://pf.kakao.com/_xaeEyT");
